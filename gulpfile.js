bbbbb988eafd9a2f853fc95d044e8f0ef8cad630
Jake Vinson <jakevinson@gmail.com>
2016-03-02 12:13:05 -0800

    Add gulp-jscs tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-var gulp = require("gulp");
+(function() {
+
+  "use strict";
+
+  var gulp = require( "gulp" );
+  var jscs = require( "gulp-jscs" );
+
+  var scripts = [
+    "**/*.js",
+    "!app/scripts/lib/*",
+    "!node_modules/*"
+  ];
+
+  gulp.task( "scripts", function() {
+    return gulp.src( scripts )
+      .pipe( jscs() )
+      .pipe( jscs.reporter() );
+  });
+
+  gulp.task( "watch", function() {
+    gulp.watch( scripts, [ "scripts" ]);
+  });
+
+})();
